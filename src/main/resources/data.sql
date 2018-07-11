f487827efeae321efd0dd2bdeac44c5b1b9762e8
Gyorfisti <gyorfisti@gmail.com>
2018-07-11 14:04:06 +0200

    add 3 test users

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO users (account_name, email, password, first_name, last_name, type) VALUES ('admin', 'admin@admin.admin', 'admin', 'admin', 'admin', 'admin');
+INSERT INTO users (account_name, email, password, first_name, last_name, type) VALUES ('Laci', 'klaszlo@admin.admin', 'admin', 'László', 'Kontra', 'admin');
+INSERT INTO users (account_name, email, password, first_name, last_name, type) VALUES ('Feri', 'hferenc@admin.admin', 'admin', 'Ferenc', 'Hajnal', 'admin');
