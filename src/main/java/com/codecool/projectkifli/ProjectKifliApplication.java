6e1fb3c8a84374620b08cfbae0bbf929feac4d9a
Gyorfisti <gyorfisti@gmail.com>
2018-07-11 20:07:30 +0200

    made the application start up

--- src/main/java/com/codecool/projectkifli/ProjectKifliApplication.java
+++ src/main/java/com/codecool/projectkifli/ProjectKifliApplication.java
@@ -2,9 +2,17 @@ package com.codecool.projectkifli;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class ProjectKifliApplication {
+
+	@RequestMapping("/")
+	String home() {
+		return "This will be the magnificent page of the Kifli Project!";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectKifliApplication.class, args);
