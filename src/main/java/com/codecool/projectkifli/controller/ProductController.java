2d90786bb111f3383978a8d191292c2f31503d26
Gyorfisti <gyorfisti@gmail.com>
2018-07-23 17:59:49 +0200

    add findById method to productcontroller

--- src/main/java/com/codecool/projectkifli/controller/ProductController.java
+++ src/main/java/com/codecool/projectkifli/controller/ProductController.java
@@ -5,20 +5,33 @@ import com.codecool.projectkifli.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/products")
 public class ProductController {
 
     @Autowired
     private ProductRepository productRepository;
 
-    @GetMapping(name = "/products",
-            produces = MediaType.APPLICATION_JSON_VALUE)
-
+    @GetMapping(
+            value = "",
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
     public List<Product> getAllProducts() {
         return productRepository.findAll();
     }
+
+    @GetMapping(
+            value = "/{id}",
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public Product findById(@PathVariable("id") Integer id) {
+        return productRepository.findById(id).orElse(null);
+    }
+
 }
